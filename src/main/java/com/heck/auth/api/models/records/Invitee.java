9b3119472b75eae98fa2a716fbf92e9cd7976971
BrookeHeck <b19heck@gmail.com>
2024-03-10 18:01:52 -0400

    add invitee record

--- src/main/java/com/heck/auth/api/models/records/Invitee.java
+++ src/main/java/com/heck/auth/api/models/records/Invitee.java
@@ -5,25 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Set;
-
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@Entity
-public class Guest {
+public class Invitee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long guestId;
+    private long inviteeId;
 
     private String firstName;
     private String lastName;
     private String prefix;
-    private String organization;
-    private String phone;
-    private String email;
-    private boolean confirmed;
-    @OneToMany(mappedBy = "guest_id")
-    private Set<Invitee> invitees;
+    @ManyToOne
+    @JoinColumn(name = "guest_id")
+    private Guest guest;
 
 }
