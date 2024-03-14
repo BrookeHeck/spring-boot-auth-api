52e282078a3118adbc89503337be526c99532fff
BrookeHeck <b19heck@gmail.com>
2024-03-14 14:44:34 -0400

    add prefix service

---
+++ src/main/java/com/heck/auth/api/services/implementations/PrefixService.java
@@ -0,0 +1,43 @@
+package com.heck.auth.api.services.implementations;
+
+import com.heck.auth.api.models.records.Prefix;
+import com.heck.auth.api.repositories.PrefixRepository;
+import com.heck.auth.api.services.DatabaseService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PrefixService implements DatabaseService<Prefix> {
+    private final PrefixRepository prefixRepository;
+
+    @Override
+    public Prefix create(Prefix record) {
+        return prefixRepository.save(record);
+    }
+
+    @Override
+    public Collection<Prefix> getAll() {
+        return prefixRepository.findAll();
+    }
+
+    @Override
+    public Prefix getOne(long id) {
+        return prefixRepository.findById(id).get();
+    }
+
+    @Override
+    public Prefix update(Prefix record) {
+        return prefixRepository.save(record);
+    }
+
+    @Override
+    public boolean delete(long id) {
+        prefixRepository.deleteById(id);
+        return true;
+    }
+}
