eafaf27fef87f1733e9429174d04dad9c627c764
BrookeHeck <b19heck@gmail.com>
2024-03-17 17:07:58 -0400

    add planner get methods

---
+++ src/main/java/com/heck/auth/api/controller/PlannerController.java
@@ -0,0 +1,26 @@
+package com.heck.auth.api.controller;
+
+import com.heck.auth.api.models.records.Planner;
+import com.heck.auth.api.services.implementations.PlannerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("planner")
+public class PlannerController {
+    private final PlannerService plannerService;
+
+    @GetMapping(path = "get/{plannerId}")
+    public Planner getPlanner(@PathVariable("plannerId") Long plannerId) {
+        return plannerService.getOne(plannerId);
+    }
+
+    @GetMapping(path = "get-all")
+    public Collection<Planner> getPlannersForEvent() {
+        return plannerService.getAll();
+    }
+}
--- src/main/java/com/heck/auth/api/repositories/PlannerRepository.java
+++ src/main/java/com/heck/auth/api/repositories/PlannerRepository.java
@@ -3,5 +3,7 @@ package com.heck.auth.api.repositories;
 import com.heck.auth.api.models.records.Planner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlannerRepository extends JpaRepository<Planner, Long> {
 }
