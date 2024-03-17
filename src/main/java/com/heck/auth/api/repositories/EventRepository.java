7f100f546bfeb5687a26a68362e0380e06cb8b2b
BrookeHeck <b19heck@gmail.com>
2024-03-17 18:38:47 -0400

    change the event repository to use long as id

--- src/main/java/com/heck/auth/api/repositories/EventRepository.java
+++ src/main/java/com/heck/auth/api/repositories/EventRepository.java
@@ -3,7 +3,5 @@ package com.heck.auth.api.repositories;
 import com.heck.auth.api.models.records.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface EventRepository extends JpaRepository<Event, UUID> {
+public interface EventRepository extends JpaRepository<Event, Long> {
 }
