11baa1e2420b7c346fee28cae3b9bed48dbaf619
BrookeHeck <b19heck@gmail.com>
2024-03-23 18:25:33 -0400

    add methods to generate jwt for user on login

--- src/main/java/com/heck/auth/api/security/JwtService.java
+++ src/main/java/com/heck/auth/api/security/JwtService.java
@@ -3,6 +3,7 @@ package com.heck.auth.api.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParserBuilder;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,12 +12,28 @@ import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
     @Value("${spring.security.secret-key")
     private String SECRET_KEY;
+
+    private String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 24)))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
     public boolean isTokenValid(String jwt, UserDetails userDetails) {
         final String username = extractUsername(jwt);
@@ -36,7 +53,7 @@ public class JwtService {
         return extractClaim(jwt, Claims::getExpiration);
     }
 
-    public <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver) {
+    private <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver) {
         final Claims claims = exractAllClaims(jwt);
         return claimsResolver.apply(claims);
     }
